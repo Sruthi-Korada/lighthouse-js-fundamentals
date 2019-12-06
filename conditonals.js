da1eeb3a49eb6411b5b52b78984e9bf93fc34597
sruthikorada36 <sruthikorada36@gmail.com>
2019-12-06 00:01:16 +0000

    save2

--- conditonals.js
+++ conditonals.js
@@ -1,7 +1,14 @@
-const raining =false;
 
-if (raining){
-console.log("dont forget your umbrella");
-}else{
-console.log("enjoy");
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
 }
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
