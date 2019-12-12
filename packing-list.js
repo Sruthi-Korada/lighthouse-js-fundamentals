334d30a247d4a6d3c4b5f93e67fab83e2d596ac4
sruthikorada36 <sruthikorada36@gmail.com>
2019-12-12 22:09:35 +0000

    packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,9 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (var i = 0; i < packingList.length; i++) {
+var i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
-console.log(packingList[3]);
+
